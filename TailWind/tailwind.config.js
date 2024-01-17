11e54fecb6631aba0d36f58a659b5cab7504ff23
Azkafar066 <151702847+Azkafar066@users.noreply.github.com>
2024-01-17 14:06:19 +0700

    update background

--- TailWind/tailwind.config.js
+++ TailWind/tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage:{
+     kucing:'url("../public/kucing.jpeg")', 
+     oyen:'url("../public/oyen.jpeg")',
+      logoalnassr:'url("../public/alnassr\ .jpg")',
+      },
       fontFamily:{
         'Lobster':'Lobster'
+      },
+      fontSize:{
+     agakbesar:"102px"   
       }
     },
   },
