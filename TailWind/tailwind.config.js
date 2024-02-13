2f9399605d3f631775d48cdd5e32362ef2e516d7
Azkafar066 <151702847+Azkafar066@users.noreply.github.com>
2024-02-13 11:14:28 +0700

    update filters

--- TailWind/tailwind.config.js
+++ TailWind/tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     extend: {
       backgroundImage: {
         kucing: 'url("../public/kucing.jpeg")',
+
         oyen: 'url("../public/oyen.jpeg")',
         logoalnassr: 'url("../public/alnassr .jpg")',
         FCBarcelona: 'url("../public/FCBarcelona.png")',
-        TarraBudiman:'url("../public/TarraBudiman.jpg")',
-          },
+        TarraBudiman: 'url("../public/TarraBudiman.jpg")',
+        priadewasa: 'url("../public/priadewasa.jpg")',
+      },
       fontFamily: {
         Lobster: "Lobster",
         Dancing: "Dancing",
+        Honk: "Honk",
       },
       fontSize: {
         agakbesar: "102px",
